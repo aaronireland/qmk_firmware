7dcabb58be69f60abc28b125939e649bc06dac9e
Aaron Ireland <aarireland@me.com>
2021-01-03 13:56:18 -0500

    Add numpad layer

--- keyboards/preonic/keymaps/aaronireland/keymap.c
+++ keyboards/preonic/keymaps/aaronireland/keymap.c
@@ -25,6 +25,7 @@ enum preonic_layers {
   _QWERTY,
   _COLEMAK,
   _DVORAK,
+  _NUMPAD,
   _LOWER,
   _RAISE,
   _ADJUST
@@ -34,6 +35,7 @@ enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
   DVORAK,
+  NUMPAD,
   LOWER,
   RAISE,
   BACKLIT
@@ -82,6 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
   KC_LGUI, KC_MPLY, KC_LALT, KC_LCTL, LOWER,   KC_SPC   ,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
+
+
+
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
@@ -103,6 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LGUI,  _______, KC_LALT, KC_LCTL, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
+
+
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * | Play |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
@@ -123,6 +130,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_HOME, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_END, \
   KC_LGUI, _______, KC_LALT, KC_LCTL, _______, _______,          _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
+
+
+
+/* Numpad
+* ,-----------------------------------------------------------------------------------.
+* | Esc  |      |      | PgDn | PgUp | Home |  End  |      |   /  |   *  |   -  | Del  |
+* |------+------+------+------+------+------+------+------+------+------+------+------|
+* | Tab  |      |  Up  |      |      |      |      |   7  |   8  |   9  |   +  | Bksp |
+* |------+------+------+------+------+-------------+------+------+------+------+------|
+* | Bksp | Left | Down | Right|      |      |      |   4  |   5  |   6  |      |  "   |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* | Shift|      |      |      |      |      |   ,  |   1  |   2  |   3  |      | Shift|
+* |------+------+------+------+------+------+------+------+------+------+------+------|
+* | GUI  |Play  | Alt  | Ctrl |Lower |    Space    |Raise |   0  |   .  | Ctrl | Shift|
+* `-----------------------------------------------------------------------------------'
+*/
+[_NUMPAD] = LAYOUT_preonic_1x2uC( \
+  KC_ESC,  _______, _______, KC_PGDN, KC_PGUP, KC_END,  KC_HOME, _______, KC_PSLS, KC_PAST, KC_PMNS, KC_BSPC,  \
+  KC_TAB,  _______, KC_UP,   _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_BSPC, \
+  KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, KC_QUOT, \
+  KC_LSFT, _______, _______, _______, _______, _______, KC_COMM, KC_P1,   KC_P2,   KC_P3,   _______, KC_RSFT, \
+  KC_LGUI, KC_MPLY, KC_LALT, KC_LCTL, LOWER,   KC_SPC   ,  RAISE,   KC_P0,   KC_PDOT, KC_RCTL, KC_RSFT \
+),
+
+
 
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
@@ -168,23 +200,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |MusOn |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |MusOff|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|AudOff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
+ * |      |      |      |Aud on|AudOff|AGnorm|AGswap|Qwerty|Numpad|Numpad|Numpad|Numpad|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
+ * |      |Voice-|Voice+|Dvorak|      |MidiOn|MidOff|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |Colemk|      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_1x2uC( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
+  MU_ON,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  MU_OFF,  \
   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,  \
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  NUMPAD, NUMPAD, NUMPAD, NUMPAD, \
+  _______, MUV_DE,  MUV_IN,  DVORAK,  _______,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
+  _______, _______, _______, COLEMAK, _______, _______, _______, _______, _______, _______, _______  \
 )
 
 
@@ -208,6 +240,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case DVORAK:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_DVORAK);
+          }
+          return false;
+          break;
+        case NUMPAD:
+          if (record->event.pressed) {
+            set_single_persistent_default_layer(_NUMPAD);
           }
           return false;
           break;
